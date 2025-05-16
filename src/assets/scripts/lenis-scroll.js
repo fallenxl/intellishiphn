9c3ff6198dac7ffe313b35e695ce82e97b54e6fd
fallxn <axlsntz.dev@gmail.com>
2025-05-15 20:37:33 -0600

    feat: Add new components and layout for Intelliship website
    - Implemented InitAnimation component for initial loading animation.
    - Added KommoChat component for customer support integration.
    - Created Map component to display service coverage.
    - Developed Navigation component with responsive design and scroll-triggered animations.
    - Introduced Plans component to showcase pricing options with animations.
    - Built Services component to highlight available services with animations.
    - Added Statistics component to display key metrics with animations.
    - Created WhyUs component to explain the benefits of using Intelliship.
    - Designed Badge component for reusable UI elements.
    - Updated Logo component with animation capabilities.
    - Added Mouse icon with animation for visual effects.
    - Established global layout structure in Layout.astro.
    - Created about and index pages to structure the website.
    - Integrated Tailwind CSS for styling and responsive design.
    - Configured TypeScript settings for strict type checking.

---
+++ src/assets/scripts/lenis-scroll.js
@@ -0,0 +1,10 @@
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
